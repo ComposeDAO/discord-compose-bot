98eb33029bb3cd8578aca56eb2e8a4c536e1a9bd
steve preston <stevepreston747@gmail.com>
2022-05-25 18:33:14 -0700

    working command

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 import "dotenv/config";
 import fs from "fs";
-// import { Client, Collection, Intents } from "discord.js";
 import { Client, Collection, Intents } from "discord.js";
 
-const prefix = process.env.PREFIX || "?";
+const PREFIX = process.env.PREFIX || "?";
 const GUILD_ID = process.env.GUILD_ID;
 const VERIFIED_ROLE_ID = process.env.VERIFIED_ROLE_ID;
 const VERIFICATION_CHANNEL_ID = process.env.VERIFICATION_CHANNEL_ID;
@@ -14,8 +13,35 @@ const client = new Client({
 });
 client.commands = new Collection();
 
+//retrieves the command files in an array
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".ts"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  // set a new item in the Collection
+  // with the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
+
 client.on("ready", function (e) {
   console.log(`Logged in as ${client.user!.tag}!`);
+});
+
+client.on("message", async (msg) => {
+  if (!msg.content.startsWith(PREFIX)) return;
+
+  const command = msg.content.substring(1);
+
+  if (!client.commands.has(command)) return;
+
+  try {
+    await client.commands.get(command).execute(msg);
+  } catch (error) {
+    console.error();
+    await msg.reply({ content: "there was an error!" });
+  }
 });
 
 client.on("interactionCreate", (interaction) => {
