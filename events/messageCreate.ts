9f78bcbcc3a5c43a5483ccc00969819212752a78
Stephen Preston <stevepreston747@gmail.com>
2022-05-26 15:40:00 -0700

    check permissions

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -1,9 +1,28 @@
+import { Permissions } from "discord.js";
+
 export const PREFIX = process.env.PREFIX || "!";
 
 module.exports = {
   name: "messageCreate",
   on: true,
   async execute(msg, client) {
+    // if (msg.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS)) {
+    //   console.log("This member has the ability to kick");
+    // }
+
+    // if (
+    //   msg.member.permissions.has(
+    //     Permissions.FLAGS.KICK_MEMBERS,
+    //     Permissions.FLAGS.BAN_MEMBERS
+    //   )
+    // ) {
+    //   console.log("This member has the ability to kick and ban");
+    // }
+
+    // if (msg.member.permissions.has(Permissions.FLAGS.KICK_MEMBERS, false)) {
+    //   console.log("This member can kick without allowing admin to override");
+    // }
+
     if (!msg.content.startsWith(PREFIX)) return;
 
     let command = msg.content.substring(1);
