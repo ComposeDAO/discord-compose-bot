482b27c816797d74e3067d6efd356044fde8a274
steve preston <stevepreston747@gmail.com>
2022-05-31 16:44:29 -0700

    remove unused

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,6 @@ import "dotenv/config";
 import fs from "fs";
 import { Client, Collection, Intents } from "discord.js";
 
-const PREFIX = process.env.PREFIX || "?";
-const GUILD_ID = process.env.GUILD_ID;
-const VERIFIED_ROLE_ID = process.env.VERIFIED_ROLE_ID;
-const VERIFICATION_CHANNEL_ID = process.env.VERIFICATION_CHANNEL_ID;
-const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
-
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
