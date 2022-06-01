c932d7afe70983e03c1896aa9c895279ef0394ad
steve preston <stevepreston747@gmail.com>
2022-05-31 17:21:11 -0700

    working verifyUser

--- utils/verifyUser.ts
+++ utils/verifyUser.ts
@@ -2,9 +2,36 @@ import { Client, Intents } from "discord.js";
 import "dotenv/config";
 
 const GUILD_ID = process.env.GUILD_ID;
+const MEMBER_ROLE_ID = process.env.MEMBER_ROLE_ID;
 
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
-export async function verifyUser(userID: string) {}
+export async function verifyUser(userID: string) {
+  const server = await client.guilds.fetch(GUILD_ID);
+  let joined = false;
+  let verified = false;
+
+  try {
+    await server.members.fetch(userID);
+    joined = true;
+  } catch (error) {
+    joined = false;
+  }
+
+  if (joined) {
+    //verify user
+    const member = await server.members.fetch(userID);
+    member.roles.add(MEMBER_ROLE_ID);
+    verified = true;
+  } else {
+    //wait till user joins
+    verified = false;
+  }
+
+  console.log(verified);
+  return verified;
+}
+
+// verifyUser("979516521166569514");
