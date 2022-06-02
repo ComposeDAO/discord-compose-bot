30e768735ff61d10d8f65f2635f2a7ee9d3af37e
steve preston <stevepreston747@gmail.com>
2022-06-01 17:19:23 -0700

    update verifyUser

--- trpc/client.ts
+++ trpc/client.ts
@@ -1,5 +1,28 @@
 import { createTRPCClient } from "@trpc/client";
 
 export const client = createTRPCClient({
-  url: "https://trpc-server.deno.dev/trpc",
+  url: "https://0ntdlvgj79.execute-api.us-east-1.amazonaws.com",
 });
+
+export async function serverEphemeral(
+  discordHandle: string,
+  clientEphemeralPublic: string
+) {
+  const response: { salt: string; serverEphemeralPublic: string } =
+    // @ts-ignore:next-line
+    await client.query("SRP-Ephemeral", {
+      discordHandle: discordHandle,
+      clientEphemeralPublic: clientEphemeralPublic,
+    });
+
+  return response;
+}
+
+export async function serverSession(clientSessionProof: string) {
+  // @ts-ignore:next-line
+  const response: string = await client.query("SRP-Session", {
+    clientSessionProof: clientSessionProof,
+  });
+
+  return response;
+}
--- utils/verifyUser.ts
+++ utils/verifyUser.ts
@@ -1,6 +1,7 @@
 import { Client, Intents } from "discord.js";
 import srp from "secure-remote-password/client";
 import "dotenv/config";
+import { serverEphemeral, serverSession } from "../trpc/client";
 
 const GUILD_ID = process.env.GUILD_ID;
 const MEMBER_ROLE_ID = process.env.MEMBER_ROLE_ID;
@@ -25,12 +26,43 @@ export async function verifyUser(nym: string, userID: string) {
     //Get member's discord handle
     const member = await server.members.fetch(userID);
     const discordHandle = member.user.username;
-
-    //
     const clientEphemeral = srp.generateEphemeral();
 
-    member.roles.add(MEMBER_ROLE_ID);
-    verified = true;
+    //Send discordHandle and clientEphemeral to server
+    //Get salt and serverEphemeral.public from server
+    const serverResult = await serverEphemeral(
+      discordHandle,
+      clientEphemeral.public
+    );
+    const salt = serverResult.salt;
+    const serverEphemeralPublic = serverResult.serverEphemeralPublic;
+
+    const privateKey = srp.derivePrivateKey(salt, "", nym);
+    const clientSession = srp.deriveSession(
+      clientEphemeral.secret,
+      serverEphemeralPublic,
+      salt,
+      "",
+      privateKey
+    );
+
+    //Send clientSession.proof to server
+    //Get serverSession.proof from server
+    const serverSessionProof = await serverSession(clientSession.proof);
+
+    //Verify and add role
+    try {
+      srp.verifySession(
+        clientEphemeral.public,
+        clientSession,
+        serverSessionProof
+      );
+
+      await member.roles.add(MEMBER_ROLE_ID);
+      verified = true;
+    } catch (error) {
+      verified = false;
+    }
   } else {
     //wait till user joins
     verified = false;
